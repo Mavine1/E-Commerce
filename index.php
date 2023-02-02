cac0c638f01b2efa303c6980ae104aec22e56e6a
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-03 01:32:01 +0300

    among stock music rabbit live

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
      integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <h1></h1>
-  <!---bootstrap js link---> 
+<!---navbar-->
+div.container-fl
+<!---bootstrap js link---> 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
   integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
