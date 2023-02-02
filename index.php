da269c62f408077418d57003fc3c6e1ab1a4184d
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-03 01:36:01 +0300

    review element ourselves cold second

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 <body>
 <!---navbar-->
 <div class="container-fluid">
-    nav
+    
 </div>
 <!---bootstrap js link---> 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" 
