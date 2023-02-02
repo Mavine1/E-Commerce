36cd5917ae12fc8b1bc960fbddb85b2cb7f056f9
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-03 01:10:01 +0300

    wheat would gentle went remain

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E commerce </title>
     <!--Bootstrap css link -->
-    <!
+    <!---font awesome link-->
 
 </head>
 <body>
-    
+  <!---bootstrap js link--->  
 </body>
 </html>
