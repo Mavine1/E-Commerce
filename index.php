1e4b43e3f0e2dbbd24f5ce67f5d2829db0cee0cf
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-04 01:34:01 +0300

    popular later social chair per

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
           <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#"><></i></a>
+          <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
           <li class="nav-item">
           <a class="nav-link" href="#">Total price:100/-</a>
         </li>
