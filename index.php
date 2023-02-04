203fcf62830c35a7a6630bfa288440006dc3c4f1
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-04 22:50:00 +0300

    dry valley income she vapor

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <div class="container-fluid p-0">
 <nav class="navbar navbar-expand-lg bg-info">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">logo</a>
+    <img src="" alt="">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
