dcc16ad5eec917ddf3577704b4e772a3018abc35
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-04 22:56:01 +0300

    expression frozen weigh way far

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 <div class="container-fluid p-0">
 <nav class="navbar navbar-expand-lg bg-info">
   <div class="container-fluid">
-    <img src="./images/logo.jpg" alt="">
+    <img src="./images/logo.jpg" alt="" class="logo">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
