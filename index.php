a694ddfc713a5e2601034d908b22a317163071c9
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-06 00:48:00 +0300

    one appropriate mail show no

--- index.php
+++ index.php
@@ -104,7 +104,7 @@
 </div>
   </div>
 <div class="col md-4">
-<div class="card" style="width: 18rem;">
+<div class="card">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
