cfc693437fbde1f268dc20dfa877b251f08bf84b
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-06 00:56:01 +0300

    jet alphabet nearby interior both

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
 </div></div>
   <div class="col-md-4">
   <div class="card">
-  <img src="./images/" class="card-img-top" alt="...">
+  <img src="./images/mango.png" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
