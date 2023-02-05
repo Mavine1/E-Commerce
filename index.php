f8afe0beac0c932f4534b19afeaace7f131304d7
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-06 01:08:01 +0300

    coal minerals spring industrial talk

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
   <div class="col md-10">
     <!--products-->
 <div class="row">
-  <div class="col md-4">
+  <div class="col md-4 mb-2">
   <div class="card">
   <img src="./images/dairy.png" class="card-img-top" alt="...">
   <div class="card-body">
@@ -84,7 +84,7 @@
   </div>
 </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-4 mb-2">
     <div class="card">
   <img src="./images/apple.png" class="card-img-top" alt="...">
   <div class="card-body">
@@ -94,7 +94,7 @@
   </div>
 </div>
 </div>
-  <div class="col-md-4">
+  <div class="col-md-4 mb-2">
   <div class="card">
   <img src="./images/mango.png" class="card-img-top" alt="...">
   <div class="card-body">
@@ -104,7 +104,17 @@
   </div>
 </div>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/apple.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
     <div class="card">
   <img src="./images/apple.png" class="card-img-top" alt="...">
   <div class="card-body">
