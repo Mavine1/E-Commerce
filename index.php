640cc1b6c71571e7d016f4bd6ddfb7744523891a
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-06 01:06:01 +0300

    wheat across faster sweet piece

--- index.php
+++ index.php
@@ -104,9 +104,29 @@
   </div>
 </div>
   </div>
+  <div class="col-md-4">
+    <div class="card">
+  <img src="./images/apple.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<div class="col-md-4 mb-2">
+    <div class="card">
+  <img src="./images/apple.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
 </div>
   </div>
-  
+
   <div class="col-md-2">
     <!--sidenav-->
   </div>
