a07da768b1be222d4a02e3fce22740c58bc77b35
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-06 23:24:00 +0300

    zulu pale porch swing barn

--- admin_area/index.php
+++ admin_area/index.php
@@ -8,13 +8,14 @@
     <!--bootstrap and css link-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" 
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <!---navbar-->
     <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light bg-info">
             <div class="container-fluid">
-                <img src="./images/logo.png" alt="">
+                <img src="../images/logo.png" alt="">
             </div>
         </nav>
     </div>
