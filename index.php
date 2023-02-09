ef5afc6a109403833156b9d428a05d2a4bd8df11
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-09 22:24:01 +0300

    afternoon twenty forget seed clothes

--- index.php
+++ index.php
@@ -148,6 +148,15 @@
       <li class="nav-item bg-info">
         <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
       </li>
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light"> Brand 1</h4></a>
+      </li>
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light">Brand 2</a>
+      </li>
+      <li class="nav-item bg-info">
+        <a href="#" class="nav-link text-light">Brands</h4></a>
+      </li>
     </ul>
   </div>
 <!---footer-->
