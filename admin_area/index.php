202087bb3b9fd08ee8cf619098fc4fea15b3ba29
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-10 22:14:00 +0300

    worker move difficulty value voyage

--- admin_area/index.php
+++ admin_area/index.php
@@ -19,7 +19,7 @@
                 <nav class="navbar navbar-expand-lg ">
                     <ul class="navbar-nav">
                         <li class ="nav-item">
-                            a.
+                            <a href="" class="nav-link">Welcome guest</a>
                         </li>
                     </ul>
                 </nav>
