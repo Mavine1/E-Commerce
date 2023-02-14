5affd8489c7ccbb90eb01fb35ec0ed5f3f4f9d07
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-14 22:02:01 +0300

    wrong grew teacher discovery than

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,7 +1,12 @@
 <form action="" method="post" class="mb-2">
 <div class="input-group w-90  mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa receipt"></i></span>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username"
+  <input type="text" class="form-control" name="cat_title"placeholder="insert categories" aria-label="Username"
+   aria-describedby="basic-addon1">
+</div>
+<div class="input-group w-90  mb-2">
+  <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa receipt"></i></span>
+  <input type="text" class="form-control" name="cat_title"placeholder="insert categories" aria-label="Username"
    aria-describedby="basic-addon1">
 </div>
 </form>
