715bd3833092b80a0305d4d45701d7aed751d544
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-17 23:24:00 +0300

    hello military south thought twice

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,7 +1,7 @@
 <form action="" method="post" class="mb-2">
 <div class="input-group w-90  mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-  <input type="text" class="form-control" name="cat_title"placeholder="insert categories" aria-label="categories"
+  <input type="text" class="form-control" name="cat_title"placeholder="insert categories" aria-label="brands"
    aria-describedby="basic-addon1">
 </div>
 <div class="input-group w-10  mb-2 m-auto">
