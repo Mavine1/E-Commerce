3602e9a215fe0bda9c552dbd2826154e029812f0
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-23 20:02:01 +0300

    wild zebra know as earn

--- admin_area/index.php
+++ admin_area/index.php
@@ -52,7 +52,7 @@
                     <button><a href="" class="nav-link text-light bg-info my-1">View products</a></button>
                     <button><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert categories</a></button>
                     <button><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
-                    <button><a href="index.php?insert_brand" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                    <button><a href="index.php?insert_brands" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
                     <button><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
                     <button><a href="" class="nav-link text-light bg-info my-1">All orders</a></button>
                     <button><a href="" class="nav-link text-light bg-info my-1">All payments</a></button>
@@ -67,7 +67,7 @@
         if(isset($_GET['insert_category'])){
             include('insert_categories.php');
         }
-        if(isset($_GET['insert_category'])){
+        if(isset($_GET['insert_brands'])){
             include('insert_categories.php');
         }
         ?>
