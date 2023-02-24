cece0a0322e298cbf2cff536b169134599a518f8
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-24 23:40:01 +0300

    pale development contrast that tall

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -2,7 +2,9 @@
 include('../includes/connect.php');
 
 ?>
-form action="" method="post" class="mb-2">
+
+
+<form action="" method="post" class="mb-2">
 <div class="input-group w-90  mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
   <input type="text" class="form-control" name="cat_title"placeholder="insert categories" aria-label="categories"
