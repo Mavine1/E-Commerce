1c804405540224216e311ae0a428e8a56f384b7c
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-02-25 23:24:01 +0300

    course bill cage told my

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -2,7 +2,8 @@
 include('../includes/connect.php');
 if(isset($_POST['insert_cart'])){
   $category_title=$_POST['cat_title'];
-  $insert_query="insert into 'categories' (category_title) ";
+  $insert_query="insert into 'categories' (category_title) values ('$category_title')";
+  $result=mysqli_query($con,)
 }
 
 ?>
