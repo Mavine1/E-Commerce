77c1c70d56b87bc6ee4ada6a57da4175dfd19e27
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-02 20:08:01 +0300

    last else temperature step metal

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -8,13 +8,13 @@ if(isset($_POST['insert_cat'])){
   $number=mysqli_num_rows($result_select);
   if($number>0){
     echo "<script>alert('Already exists in the database')</script>";
-  }
+  }else{
   $insert_query="insert into 'categories' (category_title) values ('$category_title')";
   $result=mysqli_query($con,$insert_query);
   if($result){
     echo "<script>alert('category has been inserted successfully')</script>";
   }
-}
+}}
 ?>
 
 
