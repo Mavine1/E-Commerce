6640fbcfbcafd44086426a2c935b6db8c8766a74
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-03 09:24:00 +0300

    art depend asleep labor balloon

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -5,16 +5,16 @@ if(isset($_POST['insert_cat'])){
   //select data from the database//
   $select_query="select * from 'categories' where category_title ='$category_title'";
   $result_select=mysqli_query($con,$select_query);
-  //$number=mysqli_num_rows($result_select);
-  //if($number>0){
-    //echo "<script>alert('Already exists in the database')</script>";
-  //}else{
+  $number=mysqli_num_rows($result_select);
+  if($number>0){
+    echo "<script>alert('Already exists in the database')</script>";
+  }else{
   $insert_query="insert into 'categories' (category_title) values ('$category_title')";
   $result=mysqli_query($con,$insert_query);
   if($result){
     echo "<script>alert('category has been inserted successfully')</script>";
   }
-}//}
+}}
 ?>
 
 
