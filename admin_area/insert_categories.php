d29985d3b507cabab4d00d6f276639358a202cb7
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-03 13:10:00 +0300

    period origin shelf drop offer

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -14,7 +14,17 @@ if($count>0)
   echo "<script>alert('Already exists in the database')</script>";
 
 }
-}
+
+  $row=mysqli_num_rows($result_select);
+ if($row==TRUE){
+    echo "<script>alert('Already exists in the database')</script>";
+  }else{
+  $insert_query="insert into 'categories' (Category_title) values ('$category_title')";
+  $result=mysqli_query($con,$insert_query);
+  if($result){
+    echo "<script>alert('category has been inserted successfully')</script>";
+  }
+}}
 ?>
 
 
