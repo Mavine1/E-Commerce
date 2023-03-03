0de8b4ecb904bbaed583a2b8bf5f5fc3ee03eb7d
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-03 09:42:01 +0300

    force good sitting question skin

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -1,7 +1,7 @@
 <?php
 include('../includes/connect.php');
 if(isset($_POST['insert_cart'])){
-  $category_title=$_POST['cat_title'];
+  $category_title=$_POST['cart_title'];
   //select data from the database//
   $select_query="select * from 'categories' where category_title ='$category_title'";
   $result_select=mysqli_query($con,$select_query);
@@ -9,7 +9,7 @@ if(isset($_POST['insert_cart'])){
   if($number>0){
     echo "<script>alert('Already exists in the database')</script>";
   }else{
-  $insert_query="insert into 'categories' (category_title) values ('$category_title')";
+  $insert_query="insert into 'categories' (Category_title) values ('$category_title')";
   $result=mysqli_query($con,$insert_query);
   if($result){
     echo "<script>alert('category has been inserted successfully')</script>";
@@ -21,7 +21,7 @@ if(isset($_POST['insert_cart'])){
 <form action="" method="post" class="mb-2">
 <div class="input-group w-90  mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-  <input type="text" class="form-control" name="cat_title"placeholder="insert categories" aria-label="categories"
+  <input type="text" class="form-control" name="cart_title"placeholder="insert categories" aria-label="categories"
    aria-describedby="basic-addon1">
 </div>
 <div class="input-group w-10  mb-2 m-auto">
