a8e0958b649d1ff1e7245fded355780fb96f73c8
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-03 10:02:01 +0300

    crowd greatly large beneath club

--- admin_area/insert_categories.php
+++ admin_area/insert_categories.php
@@ -3,22 +3,22 @@ include('../includes/connect.php');
 if(isset($_POST['insert_cart'])){
   $category_title=$_POST['cart_title'];
   //select data from the database//
-  $select_query="select * from 'categories' where category_title ='$category_title'";
-  $result_select=mysqli_query($con,$select_query);
-  $nuxmber=mysqli_num_rows($result_select);
-  if($number>0){
-    echo "<script>alert('Already exists in the database')</script>";
-  }else{
+  //$select_query="select * from 'categories' where category_title ='$category_title'";
+ // $result_select=mysqli_query($con,$select_query);
+  //$nuxmber=mysqli_num_rows($result_select);
+ // if($number>0){
+   // echo "<script>alert('Already exists in the database')</script>";
+  //}else{
   $insert_query="insert into 'categories' (Category_title) values ('$category_title')";
   $result=mysqli_query($con,$insert_query);
   if($result){
     echo "<script>alert('category has been inserted successfully')</script>";
   }
-}}
+}//}
 ?>
 
 
-<form action="" method="post" class="mb-2">
+<form action="" method="POST" class="mb-2">
 <div class="input-group w-90  mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
   <input type="text" class="form-control" name="cart_title"placeholder="insert categories" aria-label="categories"
