b6e5ebd55147387b51fcee52c7c6ca2ea8ff8e6b
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-04 06:54:00 +0300

    necessary activity whose dull missing

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -1,4 +1,24 @@
-<form action="" method="post" class="mb-2">
+<?php
+include('../includes/connect.php');
+if(isset($_POST['insert_cart'])){
+  $category_title=$_POST['cart_title'];
+  //select data from the database//
+  $select_query="select * from categories where category_title ='$category_title'";
+  $result_select=mysqli_query($con,$select_query);
+  $number=mysqli_num_rows($result_select);
+ if($number==TRUE){
+    echo "<script>alert('Already exists in the database')</script>";
+  }else{
+  $insert_query="insert into categories(category_title) values ('$category_title')";
+  $result=mysqli_query($con,$insert_query);
+  if($result){
+    echo "<script>alert('category has been inserted successfully')</script>";
+  }
+}}
+?>
+
+
+<form action="" method="POST" class="mb-2">
 <div class="input-group w-90  mb-2">
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
   <input type="text" class="form-control" name="cat_title"placeholder="insert brands" aria-label="brands"
