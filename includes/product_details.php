94b81b36d2acfdf3f6c481231f64f13f2420b1ef
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-19 14:06:37 +0300

    new

--- includes/product_details.php
+++ includes/product_details.php
@@ -1,7 +1,7 @@
 <!--connect file-->
 <?php
-include('../includes/connect.php');
-include('../admin_area/functions/common_function.php');
+include('includes/connect.php');
+include('admin_area/functions/common_function.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
