d6efbe5cb20c3959f7fa29bfb297e8080ff7e929
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-21 21:54:00 +0300

    story animal afraid lost glad

--- cart.php
+++ cart.php
@@ -108,9 +108,9 @@ cart();
       </tbody>
     </table>
     <!--Total-->
-    <div class="d-flex">
+    <div class="d-flex mb-5">
       <h4 class="px-3">Total:<strong class="text-info">5000/-</strong></h4>
-    <a href="index.php"><button class="bg-info px-3 py-2 border-0">Continue shopping</button></a>
+    <a href="index.php"><button class="bg-info px-3 py-2 border-0 mx-3">Continue shopping</button></a>
     <a href="#"><button class="bg-secondary px-3 py-2 border-0">Check out</button></a>
 
   </div>
