f87ece5508b686d62b7a5a8c0c7d013dc3136499
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-22 15:48:02 +0300

    dull minerals hot attention egg

--- cart.php
+++ cart.php
@@ -173,7 +173,7 @@ cart();
         $total_price</strong></h4>
         <input type='submit' value='Continue shopping'
         class='bg-info px-3 py-2 border-0 mx-3' name='continue shopping'>
-      <a href=''><button class='bg-secondary px-3 py-2 border-0 text-light'>Check out</button></a>";
+      <button class='bg-secondary px-3 py-2 border-0 text-light'><a href='checkout.php'>Check out</a></button>";
       }
       else{
         echo "<input type='submit' value='Continue shopping'
