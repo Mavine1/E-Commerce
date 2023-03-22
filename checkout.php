81080ce8c80d4819f787890602ffe493a13e1445
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-22 16:06:01 +0300

    step ability worried evidence nails

--- checkout.php
+++ checkout.php
@@ -76,43 +76,10 @@ include('includes/connect.php');
   <div class="col md-10">
     <!--products-->
 <div class="row">
-   <!--fetching data-->
-
-   <?php
-      getproducts();
-      get_unique_categories();
-      get_unique_brands();
-      //$ip = getIPAddress();  
-     //echo 'User Real IP Address - '.$ip;  
-      ?>
   <!--row-->
   </div>
   <!--col end-->
 </div>
-
-  <div class="col-md-2 bg-secondary p-0">
-    <ul class="navbar-nav me-auto text-center">
-      <li class="nav-item bg-info">
-        <a href="#" class="nav-link text-light"><h4>Delivery Brands</h4></a>
-      </li>
-
-      <!--fetching data-->
-
-      <?php
-      getbrands();
-      ?>
-    </ul>
-  <!------categories--->
-  <ul class="navbar-nav me-auto text-center">
-      <li class="nav-item bg-info">
-        <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
-      </li>
-
-      <!--fetching of data from the database-->
-      <?php
-      getcategories();
-      ?>
-    </ul>
 </div>
 <!---footer-->
 <!--include footer-->
