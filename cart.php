3c59409b41c07fc89cb7696bb757924dd390cfa6
Mavine muganda <87116248+Mavine1@users.noreply.github.com>
2023-03-22 15:32:01 +0300

    nervous highway accept letter shoot

--- cart.php
+++ cart.php
@@ -180,7 +180,7 @@ cart();
         class='bg-info px-3 py-2 border-0 mx-3' name='continue shopping'>";
       }
       if(isset($_POST['continue shopping'])){
-        echo "<script>window</script>";
+        echo "<script>window.open('index.php','_self')</script>";
       }
       ?>
       
